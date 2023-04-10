02036b4f2201f73b9e76ab2df6212e16909e51a0
tonghao <tong-hao@hotmail.com>
2023-04-10 10:11:38 +0800

    bookmark demo

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# 初始书签列表
+bookmarks = [
+    {'id': 1, 'title': 'Google', 'url': 'https://www.google.com'},
+    {'id': 2, 'title': 'Bing', 'url': 'https://www.bing.com'},
+    {'id': 3, 'title': 'Yahoo', 'url': 'https://www.yahoo.com'},
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', bookmarks=bookmarks)
+
+
+@app.route('/add_bookmark', methods=['GET', 'POST'])
+def add_bookmark():
+    if request.method == 'POST':
+        title = request.form['title']
+        url = request.form['url']
+        id = bookmarks[-1]['id'] + 1
+        if not url.startswith('http'):
+            url = 'https://' + url
+        bookmarks.append({'id': id, 'title': title, 'url': url})
+        return render_template('index.html', bookmarks=bookmarks)
+    else:
+        return render_template('add_bookmark.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
