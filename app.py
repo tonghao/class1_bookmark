e21d61dc300bbaa475c06b7330e80765badda33c
tonghao <tong-hao@hotmail.com>
2023-04-13 10:54:12 +0800

    完成用户注册功能

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = "sc-py=ps"
 
 # 初始书签列表
 bookmarks = [
-    {'id': 1, 'title': 'Google', 'url': 'https://www.google.com'},
-    {'id': 2, 'title': 'Bing', 'url': 'https://www.bing.com'},
-    {'id': 3, 'title': 'Yahoo', 'url': 'https://www.yahoo.com'},
+    {'id': 1, 'title': 'Google', 'url': 'https://www.google.com', 'user': 'admin'},
+    {'id': 2, 'title': 'Bing', 'url': 'https://www.bing.com', 'user': 'user'},
+    {'id': 3, 'title': 'Yahoo', 'url': 'https://www.yahoo.com', 'user': 'user'},
 ]
 
 users = [
@@ -17,6 +18,16 @@ users = [
 
 @app.route('/')
 def index():
+    if 'username' in session:
+        user_bookmarks = [
+            bookmark
+            for bookmark in bookmarks
+            if bookmark['user'] == session['username']
+        ]
+        return render_template('index.html', bookmarks=user_bookmarks)
+    else:
+        return redirect('/login')
+
     return render_template('index.html', bookmarks=bookmarks)
 
 
@@ -36,17 +47,29 @@ def add_bookmark():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get("username", None)
+        password = request.form.get("password", None)
+        print(username, password)
+        for user in users:
+            if user["username"] == username and user["password"] == password:
+                session['username'] = username
+                return redirect(url_for("index"))
+        flash("用户名或密码不正确")
+        return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
 @app.route('/logout')
 def logout():
-    pass
+    session.pop("username")
+    return redirect(url_for('index'))
 
 
 @app.route('/register')
 def register():
-    pass
+    return "register"
 
 
 if __name__ == '__main__':
